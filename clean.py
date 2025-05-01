801a0ab291084d11ffa8ae18876d67f01d890b49
Manas Laud <manaslaud2004@gmail.com>
2025-05-01 11:16:46 +0530

    feat: set 6

--- clean.py
+++ clean.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import re
 
-input_file = "set 5.xlsx" 
-output_file = "set5_clean.xlsx"
+input_file = "Set 6.xlsx" 
+output_file = "set6_clean.xlsx"
 
 df = pd.read_excel(input_file, engine="openpyxl")
 
